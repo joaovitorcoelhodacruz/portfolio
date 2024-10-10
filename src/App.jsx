3520bc2e5bb78f7a6368cfbcdc79ab6a9bee4edf
joaovitorcoelhodacruz <francielirott@gmail.com>
2024-10-10 20:07:18 -0300

    fazendo deploy

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Pagina(){
+  return <main>
+    <img src="./gatao.jpg" alt="Foto" width="128px"/>
+    <div>
+      <b>João Vitor Coelho Da Cruz</b> | 18 anos | Desenvolvedor Web
+    </div>
+    <div>
+      <b>Perfil</b>: Olá eu sou o João! desenvolvedor Web.
+      programo com react e estou estudando outras tecnologias
+    </div>
+    <div>
+      <b>Experiencias</b>:já participei do 
+      projeto Latidos Perdidos, estudo a 2 anos na
+      supergeeks
+    </div>
+    <div>
+      <b>competencias</b>:
+      HTML,
+      CSS,
+      javascript,
+      React,
+      mexo com computaria.
+    </div>
+    <div>
+      <b>links</b>:
+      <a href="#">instagram</a>
+      <a href="https://github/joaovitorcoelhodacruz">github</a>
+    </div>
+  </main>
+}
